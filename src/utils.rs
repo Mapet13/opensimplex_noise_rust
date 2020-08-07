7fdff535b847df7182d796d147aa0b796daed6a1
Mapet <jakubsordyl1@gmail.com>
2020-08-08 00:21:03 +0200

    clean math operation in contribute function

--- src/utils.rs
+++ src/utils.rs
@@ -11,8 +11,7 @@ pub fn contribute<NoiseEvaluatorType: NoiseEvaluator<Vec>, Vec: VecType<f64>>(
     let shifted: Vec = origin - delta - NoiseEvaluatorType::SQUISH_POINT * delta.sum();
     let attn: f64 = 2.0 - shifted.get_attenuation_factor();
     if attn > 0.0 {
-        let attn2 = attn * attn;
-        return attn2 * attn2 * NoiseEvaluatorType::extrapolate(grid + delta, shifted, perm);
+        return attn.powi(4) * NoiseEvaluatorType::extrapolate(grid + delta, shifted, perm);
     }
 
     0.0
